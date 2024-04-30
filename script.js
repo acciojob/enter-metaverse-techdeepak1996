68258ef45ed55f4a2978850e0293bf963d3143d0
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-04-30 15:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+ // Get references to the status paragraph and enter button
+    const statusParagraph = document.getElementById('status');
+    const enterButton = document.getElementById('enterBtn');
+
+    // Add click event listener to the enter button
+    enterButton.addEventListener('click', function() {
+      // Change the text content of the status paragraph
+      statusParagraph.innerHTML = '<h1>Entered Metaverse</h1>';
+    });
